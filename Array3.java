4a9a339708bfc9d94340662e773726e84f69d13e
GouthamiKoyya <149039299+GouthamiKoyya@users.noreply.github.com>
2023-10-28 12:52:33 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,7 @@
+public class Array3 { 
+  public static void main(String[] args) { 
+    Integer myInt = 10330; 
+    String myString = myInt.toString();
+    System.out.println(myString.length());
+  }
+}
